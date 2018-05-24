ed4f3d019c5348e418d84f4b476f590238593dfa
fanhl <dyguests@qq.com>
2018-05-25 07:49:08 +0800

    StarrySkyView

--- library/src/main/java/com/fanhl/dreamground/StarrySkyView.kt
+++ library/src/main/java/com/fanhl/dreamground/StarrySkyView.kt
@@ -6,6 +6,9 @@ import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
 
+/**
+ * 星空
+ */
 class StarrySkyView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
     init {
 
