b1c90a83d805f107d3a76d8bfd4c6fc45166d8b3
fanhl <dyguests@qq.com>
2018-05-26 18:42:54 +0800

    styleable

--- library/src/main/java/com/fanhl/dreamground/StarrySkyView.kt
+++ library/src/main/java/com/fanhl/dreamground/StarrySkyView.kt
@@ -16,7 +16,7 @@ class StarrySkyView @JvmOverloads constructor(context: Context, attrs: Attribute
         Paint().apply {
             style = Paint.Style.FILL_AND_STROKE
             strokeJoin = Paint.Join.ROUND
-            strokeWidth = size
+            strokeWidth = size.toFloat()
         }
     }
 
@@ -30,9 +30,9 @@ class StarrySkyView @JvmOverloads constructor(context: Context, attrs: Attribute
     /**
      * 单位单积(1px*1px)内的星星的数量
      */
-    private val density = 0.0001f
+    private var density = DENSITY_DEFAULT
     /** 星星的半径 */
-    private val size = 4f
+    private var size = 0
     private var starryAlpha = 1f
         set(value) {
             field = value
@@ -45,8 +45,11 @@ class StarrySkyView @JvmOverloads constructor(context: Context, attrs: Attribute
     init {
         val a = context.obtainStyledAttributes(attrs, R.styleable.StarrySkyView, defStyleAttr, R.style.Widget_Dreamground_StarrySkyView)
 
+        density = a.getFloat(R.styleable.StarrySkyView_density, DENSITY_DEFAULT)
+        size = a.getDimensionPixelSize(R.styleable.StarrySkyView_size, dpToPx(2))
+        starryAlpha = a.getFloat(R.styleable.StarrySkyView_starryAlpha, 1f)
 
-        starryAlpha = .8f
+        a.recycle()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -74,5 +77,13 @@ class StarrySkyView @JvmOverloads constructor(context: Context, attrs: Attribute
 //            canvas.drawCircle(x.toFloat(), y.toFloat(), size, paint)
         }
 
+    }
+
+    internal fun dpToPx(dps: Int): Int {
+        return Math.round(resources.displayMetrics.density * dps)
+    }
+
+    companion object {
+        const val DENSITY_DEFAULT = 0.0001f
     }
 }
