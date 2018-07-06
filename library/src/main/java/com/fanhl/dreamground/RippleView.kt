6176822981a753d7f1f02a6f6ea17c40adcca66d
fanhl <dyguests@qq.com>
2018-07-06 23:00:24 +0800

    test back

--- library/src/main/java/com/fanhl/dreamground/RippleView.kt
+++ library/src/main/java/com/fanhl/dreamground/RippleView.kt
@@ -1,10 +1,7 @@
 package com.fanhl.dreamground
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.PorterDuff
-import android.graphics.SurfaceTexture
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.Surface
 import android.view.TextureView
@@ -67,7 +64,7 @@ class RippleView @JvmOverloads constructor(context: Context, attrs: AttributeSet
      * 刷新Canvas
      */
     private fun updateCanvas(canvas: Canvas) {
-        canvas.drawColor(0x00000000, PorterDuff.Mode.CLEAR)
+        canvas.drawColor(Color.WHITE)
         canvas.drawRect(xx, yy, xx + 20.0f, yy + 20.0f, paint)
 
         if (xx + 20.0f + speedX >= canvas.width || xx + speedX <= 0.0f) {
