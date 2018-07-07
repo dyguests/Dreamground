b37e5eed5bce38845b003f089d9790fed4df8402
fanhl <dyguests@qq.com>
2018-07-07 18:45:14 +0800

    trace

--- library/src/main/java/com/fanhl/dreamground/RippleView.kt
+++ library/src/main/java/com/fanhl/dreamground/RippleView.kt
@@ -1,12 +1,16 @@
 package com.fanhl.dreamground
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.SurfaceTexture
 import android.support.annotation.FloatRange
 import android.support.v4.util.Pools
 import android.util.AttributeSet
 import android.view.Surface
 import android.view.TextureView
+import java.util.*
 
 /**
  * 波纹
@@ -18,9 +22,16 @@ import android.view.TextureView
 class RippleView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : TextureView(context, attrs, defStyleAttr) {
     private var renderThread: RenderThread? = null
 
+
+    private val pointPool = Pools.SynchronizedPool<Trace>(24)
+
     private val paint = Paint()
 
+    private val random = Random()
+
     // ------------------------------------------ Input ------------------------------------------
+
+    private val refreshInterval: Long
 
     private val backgroundColor: Int
 
@@ -38,17 +49,21 @@ class RippleView @JvmOverloads constructor(context: Context, attrs: AttributeSet
             }
         }
 
+    private var rippleLifetime: Long
+
     // ------------------------------------------ Operation ------------------------------------------
 
 
     init {
+        refreshInterval = REFRESH_INTERVAL_DEFAULT
+
         backgroundColor = Color.WHITE
 
         paint.color = rippleColor
 
-
         rippleRadius = 100f
         rippleRadiusFluctuation = .2f
+        rippleLifetime = 2000L
 
 
         surfaceTextureListener = object : SurfaceTextureListener {
@@ -93,20 +108,48 @@ class RippleView @JvmOverloads constructor(context: Context, attrs: AttributeSet
         canvas.drawColor(backgroundColor)
 
         // draw ripples
-        canvas.drawCircle(100f, 100f, 100f, paint)
+
+        val trace = acquireTrace()
+        try {
+            canvas.drawCircle(
+                    trace.x,
+                    trace.y,
+                    100f,
+                    paint
+            )
+        } finally {
+            releaseTrace(trace)
+        }
+    }
+
+    private fun acquireTrace(): Trace {
+        return pointPool.acquire()
+                ?.apply {
+                    x = 0f
+                    y = 0f
+                    birth = 0L
+                }
+                ?: Trace(
+                        0f,
+                        0f,
+                        0L
+                )
+    }
+
+    private fun releaseTrace(rect: Trace) {
+        rect.clear()
+        pointPool.release(rect)
     }
 
     companion object {
         /**刷新间隔*/
         private const val REFRESH_INTERVAL_DEFAULT = 20L
-
-        private val pointPool = Pools.SynchronizedPool<PointF>(24)
     }
 
     /**
      * 单独的绘制线程
      */
-    internal class RenderThread(surfaceTexture: SurfaceTexture, private val updateSurface: (Surface) -> Unit) : Thread() {
+    internal inner class RenderThread(surfaceTexture: SurfaceTexture, private val updateSurface: (Surface) -> Unit) : Thread() {
         private val surface = Surface(surfaceTexture)
 
         @Volatile
@@ -116,7 +159,7 @@ class RippleView @JvmOverloads constructor(context: Context, attrs: AttributeSet
             while (running && !Thread.interrupted()) {
                 updateSurface(surface)
                 try {
-                    sleep(REFRESH_INTERVAL_DEFAULT)
+                    sleep(refreshInterval)
                 } catch (e: InterruptedException) {
                 }
             }
@@ -125,6 +168,24 @@ class RippleView @JvmOverloads constructor(context: Context, attrs: AttributeSet
         fun stopRendering() {
             interrupt()
             running = false
+        }
+    }
+
+    /**
+     * 痕迹
+     *
+     * 用来记录一个Ripple的相关参数
+     * @param birth 诞生时间
+     */
+    private data class Trace(
+            var x: Float,
+            var y: Float,
+            var birth: Long
+    ) {
+        fun clear() {
+            x = 0f
+            y = 0f
+            birth = 0L
         }
     }
 }
