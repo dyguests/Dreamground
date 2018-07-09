8978922fb3aa0511816039151b60c6643c5d5aa3
fanhl <fanhonglin@klicen.com>
2018-07-09 10:11:13 +0800

    params

--- library/src/main/java/com/fanhl/dreamground/WaveView.kt
+++ library/src/main/java/com/fanhl/dreamground/WaveView.kt
@@ -2,7 +2,9 @@ package com.fanhl.dreamground
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.util.AttributeSet
+import android.view.Surface
 
 /**
  * 类似QQ启动页上的波浪效果那种
@@ -11,6 +13,8 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
     /** 所有浪尖 */
     private val crestss: Array<Array<Crest>>
 
+    // ------------------------------------------ Input ------------------------------------------
+
     /** 列面数 （顶点数=面数+1） */
     private var columns: Int? = null
     /** 行面数 （顶点数=面数+1） */
@@ -18,12 +22,20 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
     /** 面大小 （根据面的大小来计算出行列数） */
     private var cellSize: Float? = null
 
+    private var foreLightColor: Int
+    private var backLightColor: Int
+
+    // ------------------------------------------ Operation ------------------------------------------
+
     init {
 
         //FIXME 这里要根据宽、高、宽高、密度来计算是实际的行列数
         columns = 4
         rows = 4
         cellSize = 100f
+
+        foreLightColor = Color.WHITE
+        backLightColor = Color.BLACK
 
         crestss = Array(columns!!) {
             Array(rows!!) {
