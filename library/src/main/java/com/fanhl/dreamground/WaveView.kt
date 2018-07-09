8c0f6463f4c0cd6b857ea0733bc5ae7b71a672c9
fanhl <fanhonglin@klicen.com>
2018-07-09 09:58:09 +0800

    crestss

--- library/src/main/java/com/fanhl/dreamground/WaveView.kt
+++ library/src/main/java/com/fanhl/dreamground/WaveView.kt
@@ -4,8 +4,44 @@ import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
 
+/**
+ * 类似QQ启动页上的波浪效果那种
+ */
 class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : BaseTextureView(context, attrs, defStyleAttr) {
+    /** 所有浪尖 */
+    private val crestss: Array<Array<Crest>>
+
+    /** 列面数 （顶点数=面数+1） */
+    private var columns: Int? = null
+    /** 行面数 （顶点数=面数+1） */
+    private var rows: Int? = null
+    /** 面大小 （根据面的大小来计算出行列数） */
+    private var cellSize: Float? = null
+
+    init {
+
+        //FIXME 这里要根据宽、高、宽高、密度来计算是实际的行列数
+        columns = 4
+        rows = 4
+        cellSize = 100f
+
+        crestss = Array(columns!!) {
+            Array(rows!!) {
+                Crest()
+            }
+        }
+    }
+
     override fun updateCanvas(canvas: Canvas) {
 
+    }
+
+    /**
+     * 波峰
+     *
+     * 关键点
+     */
+    class Crest {
+        //x,y,z 都是基于初起位置的偏移值
     }
 }
