18d11c40d9206ed7bf4953083a3cc2b8e8cfc31d
fanhl <fanhonglin@klicen.com>
2019-02-26 11:43:57 +0800

    clock

--- library/src/main/java/com/fanhl/dreamground/ClockView.kt
+++ library/src/main/java/com/fanhl/dreamground/ClockView.kt
@@ -1,4 +1,18 @@
 package com.fanhl.dreamground
 
-class ClockView {
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.Nullable
+
+class ClockView(
+    context: Context,
+    @Nullable attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+    }
 }
