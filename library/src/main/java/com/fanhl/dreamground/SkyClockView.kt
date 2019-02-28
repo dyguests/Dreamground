f8b6c9a1c13c7bce4a8bfb3aeb7d6d183e353cfd
fanhl <fanhonglin@klicen.com>
2019-02-28 10:10:33 +0800

    时钟表盘

--- library/src/main/java/com/fanhl/dreamground/SkyClockView.kt
+++ library/src/main/java/com/fanhl/dreamground/SkyClockView.kt
@@ -5,6 +5,8 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.PointF
+import android.graphics.Rect
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.ColorInt
@@ -27,22 +29,39 @@ class SkyClockView @JvmOverloads constructor(
             style = Paint.Style.STROKE
         }
     }
+    private val hourTextPaint by lazy {
+        Paint()
+    }
 
     /** 时针中心坐标 */
     private val hourCenter = PointF()
     private var hourDialRadius = 0f
+    /**刻度的间距角度*/
+    var spaceAngle = 0f
+
 
     @ColorInt
     private val backgroundColor = Color.BLUE
     @Dimension(unit = Dimension.PX)
-    private val hourDialStrokeWidth = 20f
+    private val hourDialStrokeWidth = 10f
     @ColorInt
-    private val hourDialStrokeColor = Color.WHITE
+    private val hourDialColor = Color.WHITE
+    @Dimension(unit = Dimension.PX)
+    private val hourTextSize = 100f
+
+    // ---------- 临时变量区 ----------
+
+    private val tmpRect = Rect()
+    private val tmpRectF = RectF()
 
     init {
         hourDialPaint.apply {
             strokeWidth = hourDialStrokeWidth
-            color = hourDialStrokeColor
+            color = hourDialColor
+        }
+        hourTextPaint.apply {
+            textSize = hourTextSize
+            color = hourDialColor
         }
     }
 
@@ -56,7 +75,10 @@ class SkyClockView @JvmOverloads constructor(
             x = paddingLeft + validWidth / 2f
             y = paddingTop + validHeight * 1.25f
         }
-        hourDialRadius = validHeight * 0.75f
+        hourDialRadius = validHeight * 1f
+
+        hourTextPaint.getTextBounds("24", 0, 2, tmpRect)
+        spaceAngle = (tmpRect.width() * 360f / 2 / Math.PI / hourDialRadius * 1.5f/*额外空余百分比*/).toFloat()
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -67,6 +89,28 @@ class SkyClockView @JvmOverloads constructor(
     }
 
     private fun drawHourDial(canvas: Canvas) {
-        canvas.drawCircle(hourCenter.x, hourCenter.y, hourDialRadius, hourDialPaint)
+        val saveCount = canvas.save()
+
+        // 表盘刻线
+        tmpRectF.apply {
+            left = hourCenter.x - hourDialRadius
+            top = hourCenter.y - hourDialRadius
+            right = hourCenter.x + hourDialRadius
+            bottom = hourCenter.y + hourDialRadius
+        }
+
+        for (i in 0 until 12) {
+            // 表盘刻线
+            canvas.drawArc(tmpRectF, -90f + spaceAngle / 2f, 30f - spaceAngle, false, hourDialPaint)
+
+            // 表盘数字
+            val timeText = "12"
+            hourTextPaint.getTextBounds(timeText, 0, timeText.length, tmpRect)
+
+            canvas.drawText(timeText, hourCenter.x - tmpRect.exactCenterX(), hourCenter.y - hourDialRadius - tmpRect.exactCenterY(), hourTextPaint)
+            canvas.rotate(30f, hourCenter.x, hourCenter.y)
+        }
+
+        canvas.restoreToCount(saveCount)
     }
 }
